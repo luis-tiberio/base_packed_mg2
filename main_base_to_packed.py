7dcde85be6939a2f6fb1131d9d1d2d447818c200
eduardombraz <eduardo.louzada@shopee.com>
2025-08-13 15:26:24 -0300

    Update main_base_to_packed.py

--- main_base_to_packed.py
+++ main_base_to_packed.py
@@ -134,7 +134,7 @@ async def main():
             # LOGIN
             await page.goto("https://spx.shopee.com.br/")
             await page.wait_for_selector('xpath=//*[@placeholder="Ops ID"]', timeout=15000)
-            await page.locator('xpath=//*[@placeholder="Ops ID"]').fill('Ops35683')
+            await page.locator('xpath=//*[@placeholder="Ops ID"]').fill('Ops71223')
             await page.locator('xpath=//*[@placeholder="Senha"]').fill('@Shopee123')
             await page.locator('xpath=/html/body/div[1]/div/div[2]/div/div/div[1]/div[3]/form/div/div/button').click()
             await page.wait_for_timeout(15000)
